6e4eedac9d6b536141d4813beeb443127a2e01ac
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2025-12-19 10:28:20 +0700

    controllers

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'facility_id',
+        'slot_id',
+        'date',
+        'start_time',
+        'end_time',
+        'status',
+        'note',
+        'total_price',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+    }
+
+    public function slot()
+    {
+        return $this->belongsTo(Slot::class);
+    }
 }
--- app/Models/Slot.php
+++ app/Models/Slot.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slot extends Model
 {
-    //
+    protected $fillable = [
+        "facility_id",
+        "start_time",
+        "end_time",
+        "day_of_week",
+        "date",
+        "is_active"
+    ];
+
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+    }
 }
