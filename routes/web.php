4de0b124d68ece569e61e551e1681c1516344102
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2025-12-26 05:55:26 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Route::get("/bookings", [BookingController::class, 'index'])->name('bookings.index');
+//     Route::post("/bookings", [BookingController::class, 'store'])->name('bookings.store');
 require __DIR__ . '/auth.php';
