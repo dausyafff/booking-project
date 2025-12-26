661e67511eac396ef2d86c357d1c44a65226eafe
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2025-12-27 05:36:58 +0700

    ui

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers\Admin;
 
+
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+
 
 class BookingController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'ADMIN: semua booking';
+    }
+
+
+    public function approve()
+    {
+        return 'ADMIN: approve booking';
+    }
+
+
+    public function decline()
+    {
+        return 'ADMIN: decline booking';
+    }
 }
